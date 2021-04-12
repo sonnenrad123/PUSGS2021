06f8fb749a587cd445db7442fcf000cdb6a4e998
Ivan Gajic <33852380+sonnenrad123@users.noreply.github.com>
2021-04-12 13:07:15 +0200

    Restyling add incident page

--- SmartGrid2021Project/src/app/add-incident/add-incident.component.ts
+++ SmartGrid2021Project/src/app/add-incident/add-incident.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-add-incident',
@@ -17,5 +18,31 @@ export class AddIncidentComponent implements OnInit {
   toggle(param){
     this.toggledButton = param;
   }
+  
+  onIndexChanged(event : MatTabChangeEvent){
+    switch(event.index){
+      case 0:
+        this.toggledButton="BI";
+        break;
+      case 1:
+        this.toggledButton="Devices";
+        break;
+      case 2:
+        this.toggledButton="Resolution";
+        break;
+      case 3:
+        this.toggledButton="Calls";
+        break;
+      case 4:
+        this.toggledButton="Crew";
+        break;
+      case 5:
+        this.toggledButton="MA";
+        break;
+      case 6:
+        this.toggledButton="Equipment";
+        break;      
+    }
 
+  }
 }
